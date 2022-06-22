025f2a4c5d3c5d904278bea1fc962c67e0650149
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2022-06-22 13:29:52 +0530

    Create Merge two binary Max heaps.cpp

---
+++ Priority_Queue/Merge two binary Max heaps.cpp
@@ -0,0 +1,89 @@
+https://practice.geeksforgeeks.org/problems/merge-two-binary-max-heap0144/1#
+
+*************************************************SOLUTION*********************************************
+
+// { Driver Code Starts
+// Initial Template for C++
+
+// C++ program to merge two max heaps.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+    void heapify(vector<int> &a)
+    {
+        int i = a.size() - 1;
+        while(1)
+        {
+            int par_index = (i - 1) / 2;
+            if(a[i] > a[par_index])
+            {
+                int temp = a[par_index];
+                a[par_index] = a[i];
+                a[i] = temp;
+                i = par_index;
+            }
+            else
+                break;
+        }
+    }
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) 
+    {
+        for(int i = 0; i < b.size(); i++)
+        {
+            int temp = b[i];
+            a.push_back(temp);
+            heapify(a);
+        }
+        return a;
+    }
+};
+
+// { Driver Code Starts.
+
+bool isMerged(vector<int> &arr1, vector<int> &arr2, vector<int> &merged){
+    if (arr1.size() + arr2.size() != merged.size()){
+        return false;
+    }
+    arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+    sort(arr1.begin(),arr1.end());
+    vector<int> mergedCopy =merged;
+    sort(mergedCopy.begin(),mergedCopy.end());
+    if (arr1 != mergedCopy){
+        return false;
+    }
+    for(int i = 1; i<merged.size(); i++){
+        if(merged[i] > merged[(i-1)/2])
+            return false;
+    }
+    return true;
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, i;
+        cin >> n >> m;
+        vector<int> a(n,0),b(m,0);
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (i = 0; i < m; i++) {
+            cin >> b[i];
+        }
+        vector<int> merged, copyA = a, copyB = b;
+        Solution obj;
+        merged = obj.mergeHeaps(a, b, n, m);
+        bool flag = isMerged(copyA, copyB, merged);
+        if(flag == false) cout<<0<<endl;
+        else cout<<1<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
