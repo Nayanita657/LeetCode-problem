3b7548c75bdc44c757ca14afe2bf321a1019237b
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2022-07-07 19:35:44 +0530

    Create Rotate Image.cpp

---
+++ Math & Geometry/Rotate Image.cpp
@@ -0,0 +1,46 @@
+https://leetcode.com/problems/rotate-image/
+
+
+*************************************SOLUTION*****************************
+
+class Solution {
+public:
+    void transpose(vector<vector<int>>& matrix)
+    {
+        int size = matrix.size();
+        for(int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    
+    void reverserows(vector<vector<int>>& matrix)
+    {
+        for(int r = 0; r < matrix.size(); r++)
+        {
+            int left = 0;
+            int right = matrix.size() - 1;
+            while(left < right)
+            {
+                int temp = matrix[r][left];
+                matrix[r][left] = matrix[r][right];
+                matrix[r][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+    }
+    
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        int size = matrix.size();
+        transpose(matrix);
+        reverserows(matrix);
+    }
+};
