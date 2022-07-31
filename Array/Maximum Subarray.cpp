04475a3bf76e7acb01c9ad6cabc3b1e8caf4cf75
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2022-07-31 20:44:24 +0530

    Create Maximum Subarray.cpp

---
+++ Array/Maximum Subarray.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/maximum-subarray/
+
+*******************************SOLUTION******************************
+
+int maxSubArray(vector<int>& nums) 
+    {
+        int max_sum = INT_MIN;
+        int curr_sum = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            curr_sum = curr_sum + nums[i];
+            max_sum = max(max_sum, curr_sum);
+            if(curr_sum < 0)
+            {
+                //no need to add this element in future bcz it will always give -ve result
+                curr_sum = 0;
+            }
+        }
+        return max_sum;
+    }
