3e2edf7f1a93aec06d13a9b84ca9b112cfb70145
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2022-10-19 13:52:16 +0530

    Update Frog Jump.cpp

--- Dynamic Programming/Frog Jump.cpp
+++ Dynamic Programming/Frog Jump.cpp
@@ -26,7 +26,7 @@ int frogJump(int n, vector<int> &heights)
 }
 TC = O(2^N)
 
-USING MEMOIZATION(DYNAMIC PROGRAMMING)
+USING MEMOIZATION(DYNAMIC PROGRAMMING) [TOP DOWN APPROACH]
 ____________________________________________________________________________________________________________________________________________________________
 int helper(int curr_ind, vector<int> heights, vector<int>& dp)
 {
@@ -53,3 +53,27 @@ int frogJump(int n, vector<int> &heights)
     
 }  
 TC = O(N)
+SC = O(N)
+USING TABULATION(DYNAMIC PROGRAMING) [BUTTOM-UP APPROACH]
+________________________________________________________________________________________________
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> dp(n+1, -1);
+
+    // Already at last stair.
+    dp[n] = 0;
+
+    // Only choice to take a single gap jump.
+    dp[n - 1] = abs(heights[n - 2] - heights[n - 1]);
+
+    for(int i = n-2; i > -1; i--){
+
+        int oneJump = dp[i + 1] + abs(heights[i - 1] - heights[i]);
+        int twoJump = dp[i + 2] + abs(heights[i - 1] - heights[i + 1]);
+
+        dp[i] = min(oneJump, twoJump);
+    }
+
+    int ans = dp[1];
+    return ans;
+}
