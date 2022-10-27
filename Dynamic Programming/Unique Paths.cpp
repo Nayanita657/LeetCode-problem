581d6002f6702c7805444ca21fe03775f82e96e4
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2022-10-27 19:18:49 +0530

    Update Unique Paths.cpp

--- Dynamic Programming/Unique Paths.cpp
+++ Dynamic Programming/Unique Paths.cpp
@@ -1,41 +1,95 @@
 https://leetcode.com/problems/unique-paths/
 
-*****************************SOLUTION**********************************
 
-int helper(int start_row, int start_col, int target_row, int target_col, vector<vector<int>>& dp)
+https://leetcode.com/problems/unique-paths/
+
+********************************************RECURSION*********************************************
+
+int helper(int i, int j)
     {
-        //base condition
-        if(start_row > target_row || start_col > target_col)
-            return 0;
-        else if(dp[start_row][start_col] != -1)
-            return dp[start_row][start_col];
-        else if(start_row == target_row && start_col == target_col)
+        if(i == 0 && j == 0)
             return 1;
-    
+        else if(i < 0 || j < 0)
+            return 0;
         else
         {
-            //calling to right side
-            int val1 = helper(start_row, start_col + 1, target_row, target_col, dp);
-            //calling to buttom side
-            int val2 = helper(start_row + 1, start_col, target_row, target_col, dp);
-            
-            dp[start_row][start_col] = val1 + val2;
-            
-            return dp[start_row][start_col];
+            int up = helper(i - 1, j);
+            int down = helper(i, j - 1);
+            return up + down;
         }
     }
-    
     int uniquePaths(int m, int n) 
     {
-        vector<vector<int>> dp;
+        return helper(m - 1, n - 1);    
+    }
+
+TC = O(2 ^ (M*N)) WHERE M*N IS THE NUMBER OF CELLS IN THE GRID
+SC = O((M - 1) + (N - 1)) //LENGTH OF THE PATH
+
+
+*****************************************MEMOIZATION*************************************************
+int helper(int i, int j, vector<vector<int>>& dp)
+    {
+        if(i == 0 && j == 0)
+            return 1;
+        else if(i < 0 || j < 0)
+            return 0;
+        else
+        {
+            if(dp[i][j] != -1)
+                return dp[i][j];
+
+            int up = helper(i - 1, j, dp);
+            int down = helper(i, j - 1, dp);
+            dp[i][j] = up + down;
+            return dp[i][j];
+        }
+    }
+    int uniquePaths(int m, int n) 
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(m - 1, n - 1, dp);    
+    }
+TC = O(M * N)
+SC = O((M - 1) + (N - 1)) + (M * N)) //LENGTH OF PATH + 2d DP ARRAY
+**************************************TABULATION*******************************************************
+
+int helper(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int> (n, 0));
         for(int i = 0; i < m; i++)
         {
-            vector<int> inner_result;
             for(int j = 0; j < n; j++)
-                inner_result.push_back(-1);
-            
-            dp.push_back(inner_result);
+            {
+                if(i == 0 && j == 0)
+                {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                    
+
+                int up;
+                if((i - 1) < 0)
+                    up = 0;
+                else
+                    up = dp[i - 1][j];
+
+                int down;
+                if((j - 1) < 0)
+                    down = 0;
+                else
+                    down = dp[i][j - 1];
+                
+                dp[i][j] = up + down;
+
+            }
         }
-        return helper(0, 0, m - 1, n - 1,dp);
-        
+        return dp[m - 1][n - 1];
     }
+int uniquePaths(int m, int n) 
+    {        
+        return helper(m, n);  
+    }
+
+TC = O(M * N)
+SC = O(M * N)
