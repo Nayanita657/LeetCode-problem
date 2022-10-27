5446b36771da22627e4f9e76c4ea6ed21c13e08b
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2022-10-27 19:42:23 +0530

    Create Unique Paths 2.cpp

---
+++ Dynamic Programming/Unique Paths 2.cpp
@@ -0,0 +1,32 @@
+https://leetcode.com/problems/unique-paths-ii/description/
+
+*********************************MEMOIZATION*****************************************
+
+int helper(int i, int j, vector<vector<int>> obstacleGrid, vector<vector<int>>& dp)
+    {
+        if(i < 0 || j < 0)
+            return 0;
+        else if(obstacleGrid[i][j] == 1)
+            return 0;
+        else if(i == 0 && j == 0)
+            return 1;
+
+        else
+        {
+            if(dp[i][j] != -1)
+                return dp[i][j];
+
+            int up = helper(i - 1, j, obstacleGrid, dp);
+            int down = helper(i, j - 1, obstacleGrid, dp);
+            dp[i][j] = up + down;
+            return dp[i][j];
+        }
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
+    {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return helper(m - 1, n - 1, obstacleGrid, dp);
+    }
+
